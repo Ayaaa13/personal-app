1668fff6548ad45b8281fd8b07442f063ee9df8e
Ayaaa13 <aiahnava5@gmail.com>
2023-12-15 12:46:30 +0800

    Add files

---
+++ script.js
@@ -0,0 +1,44 @@
+const barsBtn = document.querySelector(".barsBtn");
+const menuModal = document.querySelector(".menu-modal");
+const overlay = document.querySelector(".overlay");
+
+barsBtn.addEventListener("click", function () {
+  menuModal.classList.add("visible");
+  overlay.classList.add("visible");
+});
+
+overlay.addEventListener("click", function () {
+  menuModal.classList.remove("visible");
+  overlay.classList.remove("visible");
+});
+
+const contactBtn = document.querySelector(".contact-button");
+const contactContent = document.querySelector(".contact-content");
+
+contactBtn.addEventListener("click", function () {
+  contactContent.classList.toggle("visible");
+});
+
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const sendmsgBtn = document.querySelector(".sendmsgBtn");
+
+sendmsgBtn.addEventListener("click", function () {
+  (function () {
+    emailjs.init("zB26cDz-RIJS8OuJq");
+  })();
+
+  let params = {
+    user_email: email.value,
+    user_message: message.value,
+  };
+
+  const ServiceID = "service_ljppj9h";
+  const TemplateID = "template_lu34r6p";
+  emailjs
+    .send(ServiceID, TemplateID, params)
+    .then((res) => {
+      alert("Email Sent");
+    })
+    .catch();
+});
