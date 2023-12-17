290ba82e07ab78287c058bb0631294d717805385
Ayaaa13 <aiahnava5@gmail.com>
2023-12-17 23:13:30 +0800

    Update

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const barsBtn = document.querySelector(".barsBtn");
-const menuModal = document.querySelector(".menu-modal");
-const overlay = document.querySelector(".overlay");
+const navbarMenuModal = document.querySelector(".navbar-menu-modal");
 
 barsBtn.addEventListener("click", function () {
-  menuModal.classList.add("visible");
-  overlay.classList.add("visible");
+  navbarMenuModal.classList.toggle("visible");
 });
 
-overlay.addEventListener("click", function () {
-  menuModal.classList.remove("visible");
-  overlay.classList.remove("visible");
-});
+// overlay.addEventListener("click", function () {
+//   menuModal.classList.remove("visible");
+//   overlay.classList.remove("visible");
+// });
 
 const contactBtn = document.querySelector(".contact-button");
 const contactContent = document.querySelector(".contact-content");
@@ -18,34 +16,11 @@ const contactOverlay = document.querySelector(".contact-overlay");
 
 contactBtn.addEventListener("click", function () {
   contactContent.classList.add("visible");
-  contactOverlay.classList.add("visible")
+  contactOverlay.classList.add("visible");
+  console.log("clicked");
 });
 
-contactOverlay.addEventListener("click", function() {
+contactOverlay.addEventListener("click", function () {
   contactContent.classList.remove("visible");
-  contactOverlay.classList.remove("visible")
-})
-
-const email = document.querySelector("#email");
-const message = document.querySelector("#message");
-const sendmsgBtn = document.querySelector(".sendmsgBtn");
-
-sendmsgBtn.addEventListener("click", function () {
-  (function () {
-    emailjs.init("zB26cDz-RIJS8OuJq");
-  })();
-
-  let params = {
-    user_email: email.value,
-    user_message: message.value,
-  };
-
-  const ServiceID = "service_ljppj9h";
-  const TemplateID = "template_lu34r6p";
-  emailjs
-    .send(ServiceID, TemplateID, params)
-    .then((res) => {
-      alert("Email Sent");
-    })
-    .catch();
+  contactOverlay.classList.remove("visible");
 });
